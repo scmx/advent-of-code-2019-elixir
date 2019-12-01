30c9aea2b670a965387104ef922e63082ad288e8
Albert Arvidsson <albert.arvidsson@gmail.com>
2019-12-01 08:03:02 +0100

    Add Day01TyrannyOfTheRocket part two

--- lib/day_01_tyranny_of_the_rocket.ex
+++ lib/day_01_tyranny_of_the_rocket.ex
@@ -9,7 +9,28 @@ defmodule Adventofcode.Day01TyrannyOfTheRocket do
     |> Enum.sum()
   end
 
+  def fuel_requirements_recursive_sum(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&fuel_requirements_recursive/1)
+    |> Enum.sum()
+  end
+
   def fuel_requirements(input) when is_number(input) do
     div(input, 3) - 2
+  end
+
+  def fuel_requirements_recursive(input) when is_number(input) do
+    calculate_fuel(0, input)
+  end
+
+  defp calculate_fuel(acc, input) when input <= 0 do
+    acc - input
+  end
+
+  defp calculate_fuel(acc, input) do
+    result = div(input, 3) - 2
+    calculate_fuel(acc + result, result)
   end
 end
--- test/day_01_tyranny_of_the_rocket_test.exs
+++ test/day_01_tyranny_of_the_rocket_test.exs
@@ -26,4 +26,21 @@ defmodule Adventofcode.Day01TyrannyOfTheRocketTest do
       assert 33583 = 100_756 |> fuel_requirements()
     end
   end
+
+  describe "fuel_requirements_recursive/1" do
+    # At first, a module of mass 1969 requires 654 fuel. Then, this fuel
+    # requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel,
+    # which requires 21 fuel, which requires 5 fuel, which requires no further
+    # fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 +
+    # 70 + 21 + 5 = 966" do
+    test "a module of mass 1969 requires 654 fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966" do
+      assert 966 = 1969 |> fuel_requirements_recursive()
+    end
+  end
+
+  describe "fuel_requirements_recursive_sum/1" do
+    test_with_puzzle_input do
+      assert 5_079_140 = puzzle_input() |> fuel_requirements_recursive_sum()
+    end
+  end
 end
