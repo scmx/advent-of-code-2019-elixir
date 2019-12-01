fdef2776bdcdf080b64360af7f1c8513a71e845b
Albert Arvidsson <albert.arvidsson@gmail.com>
2019-12-01 08:26:05 +0100

    Refactor Day01TyrannyOfTheRocket

--- lib/day_01_tyranny_of_the_rocket.ex
+++ lib/day_01_tyranny_of_the_rocket.ex
@@ -3,34 +3,36 @@ defmodule Adventofcode.Day01TyrannyOfTheRocket do
 
   def fuel_requirements_sum(input) do
     input
-    |> String.split("\n")
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.map(&fuel_requirements/1)
+    |> parse()
+    |> Enum.map(&calculate_fuel/1)
     |> Enum.sum()
   end
 
   def fuel_requirements_recursive_sum(input) do
     input
-    |> String.split("\n")
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.map(&fuel_requirements_recursive/1)
+    |> parse()
+    |> Enum.map(&calculate_fuel/1)
+    |> Enum.map(&break_down_fuel/1)
     |> Enum.sum()
   end
 
-  def fuel_requirements(input) when is_number(input) do
+  defp parse(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def calculate_fuel(input) do
     div(input, 3) - 2
   end
 
-  def fuel_requirements_recursive(input) when is_number(input) do
-    calculate_fuel(0, input)
-  end
+  def break_down_fuel(input, acc \\ 0)
 
-  defp calculate_fuel(acc, input) when input <= 0 do
-    acc - input
-  end
+  def break_down_fuel(input, acc) when input <= 0, do: acc
 
-  defp calculate_fuel(acc, input) do
-    result = div(input, 3) - 2
-    calculate_fuel(acc + result, result)
+  def break_down_fuel(input, acc) do
+    input
+    |> calculate_fuel()
+    |> break_down_fuel(input + acc)
   end
 end
--- test/day_01_tyranny_of_the_rocket_test.exs
+++ test/day_01_tyranny_of_the_rocket_test.exs
@@ -9,38 +9,38 @@ defmodule Adventofcode.Day01TyrannyOfTheRocketTest do
     end
   end
 
-  describe "fuel_requirements/1" do
-    test "For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2" do
-      assert 2 = 12 |> fuel_requirements()
-    end
-
-    test "For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2" do
-      assert 2 = 14 |> fuel_requirements()
-    end
-
-    test "For a mass of 1969, the fuel required is 654" do
-      assert 654 = 1969 |> fuel_requirements()
-    end
-
-    test "For a mass of 100756, the fuel required is 33583" do
-      assert 33583 = 100_756 |> fuel_requirements()
+  describe "fuel_requirements_recursive_sum/1" do
+    test_with_puzzle_input do
+      assert 5_079_140 = puzzle_input() |> fuel_requirements_recursive_sum()
     end
   end
 
-  describe "fuel_requirements_recursive/1" do
+  describe "calculate_fuel/1" do
+    test "For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2" do
+      assert 2 = 12 |> calculate_fuel()
+    end
+
+    test "For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2" do
+      assert 2 = 14 |> calculate_fuel()
+    end
+
+    test "For a mass of 1969, the fuel required is 654" do
+      assert 654 = 1969 |> calculate_fuel()
+    end
+
+    test "For a mass of 100756, the fuel required is 33583" do
+      assert 33583 = 100_756 |> calculate_fuel()
+    end
+  end
+
+  describe "break_down_fuel/1" do
     # At first, a module of mass 1969 requires 654 fuel. Then, this fuel
     # requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel,
     # which requires 21 fuel, which requires 5 fuel, which requires no further
     # fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 +
     # 70 + 21 + 5 = 966" do
-    test "a module of mass 1969 requires 654 fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966" do
-      assert 966 = 1969 |> fuel_requirements_recursive()
-    end
-  end
-
-  describe "fuel_requirements_recursive_sum/1" do
-    test_with_puzzle_input do
-      assert 5_079_140 = puzzle_input() |> fuel_requirements_recursive_sum()
+    test "the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966" do
+      assert 966 = 1969 |> calculate_fuel() |> break_down_fuel()
     end
   end
 end
