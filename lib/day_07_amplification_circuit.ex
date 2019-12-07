5fbd8bdbe7aa2d652119ca9a5e1c25679f9f9de3
Albert Arvidsson <albert.arvidsson@gmail.com>
2019-12-08 00:30:55 +0100

    Add Day07AmplificationCircuit part_2

--- lib/day_07_amplification_circuit.ex
+++ lib/day_07_amplification_circuit.ex
@@ -10,6 +10,12 @@ defmodule Adventofcode.Day07AmplificationCircuit do
     |> max_thruster_signal()
   end
 
+  def part_2(input) do
+    input
+    |> IntcodeComputer.parse()
+    |> max_thruster_signal_feedback()
+  end
+
   def max_thruster_signal(%Program{} = program) do
     0..4
     |> possible_amplifiers()
@@ -19,10 +25,33 @@ defmodule Adventofcode.Day07AmplificationCircuit do
     |> Enum.max()
   end
 
+  def max_thruster_signal_feedback(%Program{} = program) do
+    5..9
+    |> possible_amplifiers()
+    |> Enum.map(&run_with_feedback(&1, Program.fallback_input(program, nil)))
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.map(&IntcodeComputer.output/1)
+    |> Enum.max()
+  end
+
   def run_with_amplifiers(amplifiers, program) do
     amplifiers
     |> Enum.map(&Program.input(program, &1))
     |> Enum.map_reduce(0, &run/2)
+  end
+
+  def run_with_feedback(amplifiers, program) do
+    amplifiers
+    |> run_with_amplifiers(program)
+    |> do_run_with_feedback()
+  end
+
+  def do_run_with_feedback({_, %Program{status: :halted}} = result), do: result
+
+  def do_run_with_feedback({programs, %Program{output: output}}) do
+    programs
+    |> Enum.map_reduce(output, &run/2)
+    |> do_run_with_feedback()
   end
 
   defp run(program, %Program{output: output}) do
--- test/day_07_amplification_circuit_test.exs
+++ test/day_07_amplification_circuit_test.exs
@@ -22,9 +22,30 @@ defmodule Adventofcode.Day07AmplificationCircuitTest do
     end
   end
 
+  describe "max_thruster_signal_feedback/1" do
+    @input ~i[3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+    test "max thruster signal 139629729 (from phase setting sequence 9,8,7,6,5)" do
+      assert 139_629_729 = @input |> Program.new() |> max_thruster_signal_feedback()
+    end
+
+    @input ~i[3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
+    test "max thruster signal 18216 (from phase setting sequence 9,7,8,5,6)" do
+      assert 18216 = @input |> Program.new() |> max_thruster_signal_feedback()
+    end
+  end
+
   describe "part_1/1" do
     test_with_puzzle_input do
       assert 20413 = puzzle_input() |> part_1()
+    end
+  end
+
+  describe "part_2/1" do
+    test_with_puzzle_input do
+      assert 3_321_777 = puzzle_input() |> part_2()
     end
   end
 end
