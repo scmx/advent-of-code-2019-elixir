547ba8c1f31afe3393b8d225b5ed6e97fd0f7f02
Albert Arvidsson <albert.arvidsson@gmail.com>
2019-12-08 07:40:06 +0100

    Add Day08SpaceImageFormat part_2

--- lib/day_08_space_image_format.ex
+++ lib/day_08_space_image_format.ex
@@ -10,6 +10,11 @@ defmodule Adventofcode.Day08SpaceImageFormat do
     |> least_corrupted_layer({width, height})
   end
 
+  def part_2(input, {width, height}) do
+    input
+    |> render_pixels({width, height})
+  end
+
   def least_corrupted_layer(input, {width, height}) do
     input
     |> String.graphemes()
@@ -18,6 +23,35 @@ defmodule Adventofcode.Day08SpaceImageFormat do
     |> Enum.min_by(&count(&1, @black))
     |> sum_ones_and_twos()
   end
+
+  def render_pixels(input, {width, height}) do
+    input
+    |> String.graphemes()
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.chunk_every(width * height)
+    |> List.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(&render_pixel/1)
+    |> Enum.chunk_every(width)
+    |> Enum.map(&Enum.join/1)
+    |> Enum.map(&String.trim_trailing/1)
+    |> Enum.join("\n")
+    |> do_render()
+  end
+
+  defp do_render(output) do
+    IO.puts(output)
+    output
+  end
+
+  defp render_pixel(pixel_layers) do
+    pixel_layers
+    |> Enum.map(&replace_black_with_space/1)
+    |> Enum.find(&(&1 != @transparent))
+  end
+
+  defp replace_black_with_space(@black), do: " "
+  defp replace_black_with_space(other), do: other
 
   defp count(layer, value) do
     layer
--- test/day_08_space_image_format_test.exs
+++ test/day_08_space_image_format_test.exs
@@ -1,6 +1,7 @@
 defmodule Adventofcode.Day08SpaceImageFormatTest do
   use Adventofcode.FancyCase
 
+  import ExUnit.CaptureIO
   import Adventofcode.Day08SpaceImageFormat
 
   describe "least_corrupted_layer/1" do
@@ -9,9 +10,37 @@ defmodule Adventofcode.Day08SpaceImageFormatTest do
     end
   end
 
+  describe "render_pixels/1" do
+    @output ~h"""
+     1
+    1
+    """
+    test "0222112222120000" do
+      fun = fn -> "0222112222120000" |> part_2({2, 2}) end
+
+      assert capture_io(fun) == @output
+    end
+  end
+
   describe "part_1/1" do
     test_with_puzzle_input do
       assert 1441 = puzzle_input() |> part_1({25, 6})
+    end
+  end
+
+  describe "part_2/1" do
+    @output ~h"""
+    111  1  1 1111 111  111
+    1  1 1  1    1 1  1 1  1
+    1  1 1  1   1  111  1  1
+    111  1  1  1   1  1 111
+    1 1  1  1 1    1  1 1
+    1  1  11  1111 111  1
+    """
+    test_with_puzzle_input do
+      fun = fn -> puzzle_input() |> part_2({25, 6}) end
+
+      assert capture_io(fun) == @output
     end
   end
 end
