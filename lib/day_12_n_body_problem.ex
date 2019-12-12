bdf5e36181bb9630ef244d2847f17ed931d2ca79
Albert Arvidsson <albert.arvidsson@gmail.com>
2019-12-12 08:52:14 +0100

    Add Day12NBodyProblem part_1

---
+++ lib/day_12_n_body_problem.ex
@@ -0,0 +1,151 @@
+defmodule Adventofcode.Day12NBodyProblem do
+  use Adventofcode
+
+  @moduledoc """
+  Each moon has a 3-dimensional position (x, y, and z)
+  and a 3-dimensional velocity.
+
+  The position of each moon is given in your scan;
+  the x, y, and z velocity of each moon starts at 0.
+
+  Simulate the motion of the moons in time steps.
+  Within each time step, first update the velocity of every moon by applying gravity.
+  Then, once all moons' velocities have been updated,
+  update the position of every moon by applying velocity.
+  Time progresses by one step once all of the positions are updated.
+
+  ### GRAVITY
+  To apply gravity, consider every pair of moons. On each axis (x, y, and z),
+  the velocity of each moon changes by exactly +1 or -1 to pull the moons together.
+  For example, if Ganymede has an x position of 3, and Callisto has a x position of 5,
+  then Ganymede's x velocity changes by +1 (because 5 > 3)
+  and Callisto's x velocity changes by -1 (because 3 < 5).
+  However, if the positions on a given axis are the same,
+  the velocity on that axis does not change for that pair of moons.
+
+  Once all gravity has been applied, apply velocity: simply add the velocity of
+  each moon to its own position. For example, if Europa has a position of x=1, y=2, z=3
+  and a velocity of x=-2, y=0,z=3, then its new position would be x=-1, y=2, z=6.
+  This process does not modify the velocity of any moon.
+  """
+
+  alias __MODULE__.{Parser, Planet, Position, Printer, Moon, Velocity}
+
+  def part_1(input) do
+    input
+    |> Parser.parse()
+    |> Planet.simulate_step(1000)
+    |> Planet.total_energy()
+  end
+
+  defmodule Moon do
+    defstruct pos: {0, 0, 0}, vel: {0, 0, 0}
+  end
+
+  defmodule Planet do
+    defstruct step: 0, moons: []
+
+    def simulate_step(%Planet{step: step} = planet, step), do: planet
+
+    def simulate_step(%Planet{step: step} = planet, until_step) when step < until_step do
+      planet
+      |> Velocity.update_moons()
+      |> Position.update_moons()
+      |> Map.update!(:step, &(&1 + 1))
+      |> simulate_step(until_step)
+    end
+
+    def total_energy(planet) do
+      planet.moons
+      |> Enum.map(&energy_moon/1)
+      |> Enum.sum()
+    end
+
+    defp energy_moon(moon) do
+      energy(moon.pos) * energy(moon.vel)
+    end
+
+    defp energy(values) do
+      values
+      |> Tuple.to_list()
+      |> Enum.map(&abs/1)
+      |> Enum.sum()
+    end
+  end
+
+  defmodule Velocity do
+    def update_moons(%Planet{moons: moons} = planet) do
+      %{planet | moons: moons |> Enum.map(&update_moon(&1, moons -- [&1]))}
+    end
+
+    defp update_moon(moon, moons) do
+      %{moon | vel: determine_velocity(moon, moons)}
+    end
+
+    defp determine_velocity(moon, moons) do
+      [0, 1, 2]
+      |> Enum.map(&update_velocity_dimension(moon, &1, moons))
+      |> List.to_tuple()
+    end
+
+    defp update_velocity_dimension(%Moon{pos: pos, vel: vel}, index, moons) do
+      other_vals = Enum.map(moons, &elem(&1.pos, index))
+
+      update_velocity_value(elem(vel, index), elem(pos, index), other_vals)
+    end
+
+    defp update_velocity_value(vel, pos, other_vals) do
+      Enum.reduce(other_vals, vel, fn
+        other_pos, acc when other_pos > pos -> acc + 1
+        other_pos, acc when other_pos < pos -> acc - 1
+        _, acc -> acc
+      end)
+    end
+  end
+
+  defmodule Position do
+    def update_moons(%Planet{moons: moons} = planet) do
+      %{planet | moons: moons |> Enum.map(&update_moon/1)}
+    end
+
+    defp update_moon(moon) do
+      %{moon | pos: next_pos(moon)}
+    end
+
+    defp next_pos(moon) do
+      [0, 1, 2]
+      |> Enum.map(&(elem(moon.pos, &1) + elem(moon.vel, &1)))
+      |> List.to_tuple()
+    end
+  end
+
+  defmodule Printer do
+    def print(planet) do
+      IO.puts("After #{planet.step} steps:\n#{print_moons(planet)}")
+    end
+
+    defp print_moons(%Planet{moons: moons}) do
+      moons |> Enum.map_join("\n", &print_moon/1)
+    end
+
+    defp print_moon(%Moon{pos: {x, y, z}, vel: {xv, yv, zv}}) do
+      :io_lib.format("pos=<x=~3b, y=~3b, z=~3b>, vel=<x=~3b, y=~3b, z=~3b>", [x, y, z, xv, yv, zv])
+    end
+  end
+
+  defmodule Parser do
+    def parse(input) do
+      %Planet{moons: parse_moons(input)}
+    end
+
+    defp parse_moons(input) do
+      ~r/-?\d+/
+      |> Regex.scan(input)
+      |> List.flatten()
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.chunk_every(3)
+      |> Enum.map(&List.to_tuple/1)
+      |> Enum.map(&%Moon{pos: &1})
+    end
+  end
+end
---
+++ test/day_12_n_body_problem_test.exs
@@ -0,0 +1,90 @@
+defmodule Adventofcode.Day12NBodyProblemTest do
+  use Adventofcode.FancyCase
+
+  alias Adventofcode.Day12NBodyProblem.{Parser, Planet, Printer}
+
+  import Adventofcode.Day12NBodyProblem
+  import ExUnit.CaptureIO
+
+  describe "suppose your scan reveals the following positions" do
+    @input """
+    <x=-1, y=0, z=2>
+    <x=2, y=-10, z=-7>
+    <x=4, y=-8, z=8>
+    <x=3, y=5, z=-1>
+    """
+
+    @expected """
+    After 0 steps:
+    pos=<x= -1, y=  0, z=  2>, vel=<x=  0, y=  0, z=  0>
+    pos=<x=  2, y=-10, z= -7>, vel=<x=  0, y=  0, z=  0>
+    pos=<x=  4, y= -8, z=  8>, vel=<x=  0, y=  0, z=  0>
+    pos=<x=  3, y=  5, z= -1>, vel=<x=  0, y=  0, z=  0>
+    """
+    test "printing after 0 steps" do
+      fun = fn ->
+        @input |> Parser.parse() |> Printer.print()
+      end
+
+      assert @expected == capture_io(fun)
+    end
+
+    @expected_1 """
+    After 1 steps:
+    pos=<x=  2, y= -1, z=  1>, vel=<x=  3, y= -1, z= -1>
+    pos=<x=  3, y= -7, z= -4>, vel=<x=  1, y=  3, z=  3>
+    pos=<x=  1, y= -7, z=  5>, vel=<x= -3, y=  1, z= -3>
+    pos=<x=  2, y=  2, z=  0>, vel=<x= -1, y= -3, z=  1>
+    """
+    test "printing after simulating 1 step" do
+      fun = fn ->
+        @input
+        |> Parser.parse()
+        |> Planet.simulate_step(1)
+        |> Printer.print()
+      end
+
+      assert @expected_1 == capture_io(fun)
+    end
+
+    @expected_10 """
+    After 10 steps:
+    pos=<x=  2, y=  1, z= -3>, vel=<x= -3, y= -2, z=  1>
+    pos=<x=  1, y= -8, z=  0>, vel=<x= -1, y=  1, z=  3>
+    pos=<x=  3, y= -6, z=  1>, vel=<x=  3, y=  2, z= -3>
+    pos=<x=  2, y=  0, z=  4>, vel=<x=  1, y= -1, z= -1>
+    """
+    test "printing after simulating 10 steps" do
+      fun = fn ->
+        @input
+        |> Parser.parse()
+        |> Planet.simulate_step(10)
+        |> Printer.print()
+      end
+
+      assert @expected_10 == capture_io(fun)
+    end
+
+    test "total_energy after 10 steps" do
+      assert 179 = @input |> Parser.parse() |> Planet.simulate_step(10) |> Planet.total_energy()
+    end
+  end
+
+  describe "here's a second example" do
+    @input """
+    <x=-8, y=-10, z=0>
+    <x=5, y=5, z=10>
+    <x=2, y=-7, z=3>
+    <x=9, y=-8, z=-3>
+    """
+    test "total_energy after 100 steps" do
+      assert 1940 = @input |> Parser.parse() |> Planet.simulate_step(100) |> Planet.total_energy()
+    end
+  end
+
+  describe "part_1/1" do
+    test_with_puzzle_input do
+      assert 8044 = puzzle_input() |> part_1()
+    end
+  end
+end
